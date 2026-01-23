2b44734cd680dd4e4c35e0dd6c2eda2f23069372
shivam jaiswal <cvam020801@gmail.com>
2026-01-23 12:41:23 +0530

    Time: 6 ms (6.50%) | Memory: 43.1 MB (25.67%) - LeetSync

---
+++ java/1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+         sentence = sentence.toLowerCase();
+            HashSet<Character> set = new HashSet<>();
+            for(int i=0;i<sentence.length();i++)
+            {
+                char ch = sentence.charAt(i);
+                if(ch>='a' && ch<='z')
+                {
+                    set.add(ch);
+                }
+            }
+            return set.size()==26;
+    }
+}
